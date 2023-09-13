b47fa40d7cc6aa2ba51c4f43dcb41805b6ca6de1
Izabela <izabelasilva07@hotmail.com>
2023-09-13 10:48:28 -0300

    aula 01

---
+++ aula01.py
@@ -0,0 +1,11 @@
+#input = recebe dados
+#print = mostra
+# \n = quebra linha, mas só funciona dentro do texto
+
+nome = input("Qual seu nome? ")
+nome1 = input("Qual seu nome? ")
+nome2 = input("Qual seu nome? ")
+
+idade = 17
+#print("Vocês se chamam:",nome,"\n",nome1,"\n",nome2)
+print(f"Olá {nome} \n {nome1} \n {nome2} parabéns")
