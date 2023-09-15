a849b8889ee7bcf20d6c091d4a76339712d96bfb
Izabela <izabelasilva07@hotmail.com>
2023-09-15 09:38:34 -0300

    exercicio 5

---
+++ exercicio09.py
@@ -0,0 +1,43 @@
+#for x in  range(0,10,1)
+#for (x=1;x<10;x++)
+# print(x,end=" ") mostra na mesma linha ex: 1 2 3
+
+#for x in range(0,10,1):
+#    print(x,end=" ") ordem crescente
+
+#for x in range(10,0,-1):
+    #print(x,end=" ") ordem decre
+
+#for x in range(1,11,1):
+#    print(x, end=" ")
+#for x in range(9,0,-1):
+#    print(x, end=" ")
+"""x = int(input("Digite um numero: "))
+for y in range(0,x*11,x*1):
+    print(f"{x} x {y} = {y}")
+
+r = int(input("Digite um numero: "))
+for x in range(101,101+r,1):
+    print(x, end=" ")"""
+
+n = int(input("Digite um numero: "))
+if n ==0:
+    print("Numero invalido")
+for x in range(1,n+1,1):
+    print(x, end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- laços.py
+++ laços.py
@@ -1,4 +1,6 @@
 #for x in  range(0,10,1)
+#for (x=1;x<10;x++)
+
 soma = 0
 for x in  range(10):
     num = float(input("Digite um número: "))
