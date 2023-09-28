3a8c4b28058017b02d04fae8c0733e927521b7eb
Izabela <izabelasilva07@hotmail.com>
2023-09-28 11:59:04 -0300

    revisão ex 7

---
+++ ex7.py
@@ -0,0 +1,10 @@
+base = 0
+altura = 0
+while base == 0:
+    base = float(input("Qual a base: "))
+while altura ==0:
+    altura = float(input("Qual a altura: "))
+
+area = (base * altura)/2
+
+print(f"A área do triangulo é {area}")
