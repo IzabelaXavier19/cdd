f739b2b3e9f8e228d70772761ef40212476892c4
Izabela <izabelasilva07@hotmail.com>
2023-09-28 11:56:56 -0300

    revisão ex 1

---
+++ ex1.py
@@ -0,0 +1,6 @@
+#receberr 2 notas e calcular media
+nota1 = float(input("Qual a primeira nota: "))
+nota2 = float(input("Qual a primeira nota: "))
+
+media = nota1+nota2/2
+print(f"Sua media é {media}")
