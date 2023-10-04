97c3f8b820064276e0949e68842f5036ab036d89
IzabelaXavier19 <97764908+IzabelaXavier19@users.noreply.github.com>
2023-10-04 00:23:24 -0300

    ex12.py
    Área do retângulo

---
+++ ex12.py
@@ -0,0 +1,10 @@
+base = 0
+altura = 0
+while base == 0:
+    base = float(input("Qual a base: "))
+while altura ==0:
+    altura = float(input("Qual a altura: "))
+
+area = (base * altura)
+
+print(f"A área do retangulo é {area}")
